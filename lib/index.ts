1de53ae639cae39229ae3b0460bcfea2674b0c15
Alex Hicks <aldahick@iupui.edu>
2017-09-11 07:57:12 -0400

    Implement "generate indexes" action

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import * as childProcess from "child_process";
 import * as path from "path";
+import * as recursiveReaddirCallback from "recursive-readdir";
 import * as util from "util";
 
 export const CLI_DIR = path.join(__dirname, "../..").replace(/\\/g, "/");
@@ -8,3 +9,4 @@ export const COMPILER_PATH = CLI_DIR + "/node_modules/typescript/bin/tsc";
 export const WORKING_DIR = process.cwd().replace(/\\/g, "/");
 
 export const exec = util.promisify(childProcess.exec);
+export const recursiveReaddir: (path: string) => Promise<string[]> = <any>util.promisify(recursiveReaddirCallback);
