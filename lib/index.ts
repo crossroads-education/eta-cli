35606bbe93498b9e30b6c4bb254cedc024e22e2f
Alex Hicks <aldahick@iupui.edu>
2017-09-18 12:34:47 -0400

    Implement forEachClientJS and transformJsonFile

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 import * as childProcess from "child_process";
+import * as fs from "fs-extra";
 import * as Github from "github";
 import * as os from "os";
 import * as path from "path";
@@ -18,6 +19,31 @@ export const github = new Github({
     protocol: "https"
 });
 export const recursiveReaddir: (path: string) => Promise<string[]> = <any>util.promisify(recursiveReaddirCallback);
+
+export async function forEachClientJS(moduleNames: string[], worker: (jsDir: string) => Promise<boolean>): Promise<boolean> {
+    if (moduleNames.length === 0) {
+        moduleNames = await fs.readdir(WORKING_DIR + "/modules");
+    }
+    let success = true;
+    for (const moduleName of moduleNames) {
+        const moduleDir: string = WORKING_DIR + "/modules/" + moduleName;
+        const moduleConfig: ModuleConfiguration = await fs.readJSON(moduleDir + "/eta.json");
+        for (const staticDir of moduleConfig.dirs.staticFiles) {
+            const jsDir = `${moduleDir}/${staticDir}/js`;
+            if (!(await fs.pathExists(jsDir + "/tsconfig.json"))) {
+                continue;
+            }
+            if (!await worker(jsDir)) {
+                success = false;
+            }
+        }
+    }
+    return success;
+}
+
+export async function transformJsonFile(filename: string, worker: (original: any) => any): Promise<void> {
+    return await fs.writeJSON(filename, worker(await fs.readJSON(filename)));
+}
 
 export interface ModuleConfiguration {
     /**
