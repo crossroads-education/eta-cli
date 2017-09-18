eaa89daa42a6aee6bebc19e863697f12cc9d0caa
Alex Hicks <aldahick@iupui.edu>
2017-09-18 12:35:01 -0400

    Use forEachClientJS in "compile client"

--- lib/actions/compile/client.ts
+++ lib/actions/compile/client.ts
@@ -1,39 +1,20 @@
 import * as fs from "fs-extra";
 import * as lib from "../..";
 
-async function compile(moduleName: string): Promise<boolean> {
-    const moduleDir: string = lib.WORKING_DIR + "/modules/" + moduleName;
-    const moduleConfig: lib.ModuleConfiguration = await fs.readJSON(moduleDir + "/eta.json");
-    let success = true;
-    for (const staticDir of moduleConfig.dirs.staticFiles) {
-        const jsDir = `${moduleDir}/${staticDir}/js`;
-        if (!(await fs.pathExists(jsDir + "/tsconfig.json"))) {
-            continue;
-        }
-        try {
-            await lib.exec("node " + lib.COMPILER_PATH, {
-                cwd: jsDir
-            });
-        } catch (err) {
-            process.stderr.write(err.stdout);
-            success = false;
-        }
+async function compile(jsDir: string): Promise<boolean> {
+    console.log(`\tCompiling "${jsDir}"...`);
+    try {
+        await lib.exec("node " + lib.COMPILER_PATH, {
+            cwd: jsDir
+        });
+        return true;
+    } catch (err) {
+        process.stderr.write(err.stdout);
+        return false;
     }
-    return success;
 }
 
-export default async function execute(allowedModuleNames: string[]): Promise<boolean> {
+export default async function execute(moduleNames: string[]): Promise<boolean> {
     console.log("Compiling client-side JS...");
-    let moduleNames: string[] = await fs.readdir(lib.WORKING_DIR + "/modules");
-    if (allowedModuleNames.length > 0) {
-        moduleNames = moduleNames.filter(name => allowedModuleNames.includes(name));
-    }
-    let success = true;
-    for (const name of moduleNames) {
-        console.log(`\tCompiling "${name}"...`);
-        if (!(await compile(name))) {
-            success = false;
-        }
-    }
-    return success;
+    return lib.forEachClientJS(moduleNames, compile);
 }
