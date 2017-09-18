c0e1ca16d655515c7e312ca9b54036127ca4eb5b
Alex Hicks <aldahick@iupui.edu>
2017-09-18 08:30:13 -0400

    Implement "setup" action

---
+++ lib/actions/setup.ts
@@ -0,0 +1,19 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as lib from "..";
+import compileServer from "./compile/server";
+import generateIndexes from "./generate/indexes";
+
+export default async function execute(args: string[]): Promise<boolean> {
+    console.log("Setting up fresh Eta instance...");
+    await lib.exec("npm install");
+    await generateIndexes([]);
+    await compileServer([], false); // it'll throw unnecessary errors about db.ts, don't need to scare the user
+    const configFiles: string[] = await lib.recursiveReaddir(lib.WORKING_DIR + "/config");
+    await Promise.all(configFiles
+        .filter(f => f.endsWith(".sample.json"))
+        .map(f => fs.copy(f, f.replace(/\.sample\.json/g, ".json")))
+    );
+    console.log("\tPlease make sure to edit the config files!");
+    return true;
+}
