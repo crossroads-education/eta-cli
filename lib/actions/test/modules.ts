99e18b91ab133970931b9f41af52fbddd557a420
Alex Hicks <alex@alexhicks.net>
2018-03-01 07:41:10 -0500

    Refactor `test modules` to work better

--- lib/actions/test/modules.ts
+++ lib/actions/test/modules.ts
@@ -1,6 +1,8 @@
 import * as childProcess from "child_process";
+import * as crypto from "crypto";
 import * as fs from "fs-extra";
 import * as lib from "../..";
+import * as Mocha from "mocha";
 import * as request from "request-promise";
 import resetDB from "../db/reset";
 import seedDB from "../db/seed";
@@ -15,7 +17,28 @@ export default async function execute(args: string[]): Promise<boolean> {
     if (shouldReset) await resetDB(["--no-wait"]);
     const serverProcess = await lib.startChildServer(shouldLogAll);
     if (shouldReset) await seedDB([serverProcess]);
-    await lib.sendChildMessage(serverProcess, "eta:cre:test");
-    serverProcess.kill();
-    return true;
+    const mocha = new Mocha();
+    const moduleDir = lib.WORKING_DIR + "/modules/";
+    (await Promise.all<string[]>((await fs.readdir(moduleDir)).map(async f => {
+        const testDir = moduleDir + f + "/test";
+        if (!await fs.pathExists(testDir)) return [];
+        return await lib.recursiveReaddir(testDir);
+    }))).reduce((p, v) => p.concat(v), []).filter(f => f.endsWith(".js")).forEach(f => mocha.addFile(f));
+    const db = await lib.connectDB();
+    let apiToken = "";
+    if (shouldReset) {
+        const dbResult = await db.query("select user_id as id from user_position limit 1");
+        apiToken = crypto.randomBytes(16).toString("hex");
+        await db.query(`update "user" set "api_token" = $1::text where "id" = $2::int`, [apiToken, dbResult.rows[0].id]);
+    } else {
+        const dbResult = await db.query(`select api_token as "apiToken" from "user" where api_token is not null limit 1`);
+        apiToken = dbResult.rows[0].apiToken;
+    }
+    process.env.API_TOKEN = apiToken;
+    return await new Promise<boolean>((resolve, reject) => {
+        mocha.run(failureCount => {
+            serverProcess.kill();
+            resolve(failureCount === 0);
+        });
+    });
 }
