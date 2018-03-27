64bfece23fe24e1fef53f97caeb93c2786903745
Alex Hicks <alex@alexhicks.net>
2018-03-27 15:58:24 -0400

    Add boilerplate `generate:indexes` (so `generate:clientjs` will work)

---
+++ src/commands/generate/indexes.ts
@@ -0,0 +1,9 @@
+import * as oclif from "@oclif/command";
+
+export default class GenerateIndexes extends oclif.Command {
+    static description = "generate index files";
+
+    async run() {
+        // TODO implement
+    }
+}
