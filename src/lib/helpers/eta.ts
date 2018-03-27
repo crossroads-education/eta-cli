ecb494c20a7a6eac3d30ce314162f6933b61ef74
Alex Hicks <alex@alexhicks.net>
2018-03-27 13:22:08 -0400

    Implement `compile:client`

--- src/lib/helpers/eta.ts
+++ src/lib/helpers/eta.ts
@@ -9,4 +9,21 @@ export default class HelperEta {
         await client.connect();
         return client;
     }
+
+    public static get workingModuleName(): string {
+        const tokens: string[] = process.cwd().replace(/\\/g, "/").split("/");
+        return tokens.reverse()[tokens.reverse().findIndex(t => t === "modules") + 1];
+    }
+
+    public static async getClientJSDirs(workingDir: string, moduleNames: string[]): Promise<string[]> {
+        if (moduleNames.length === 0) moduleNames = await fs.readdir(workingDir + "/modules");
+        return (await Promise.all(moduleNames.map(async moduleName => {
+            const moduleDir = workingDir + "/modules/" + moduleName;
+            const moduleConfig = await fs.readJSON(moduleDir + "/eta.json");
+            return <string[]>(await Promise.all((<string[]>moduleConfig.dirs.staticFiles).map(async staticDir => {
+                const jsDir = `${moduleDir}/${staticDir}/js`;
+                return (await fs.pathExists(jsDir + "/tsconfig.json")) ? jsDir : undefined;
+            }))).filter(d => d !== undefined);
+        }))).reduce((p, v) => p.concat(v));
+    }
 }
