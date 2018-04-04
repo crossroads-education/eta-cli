864931c7ac7d73ee52af1a0acb6589f8d3103af7
Alex Hicks <alex@alexhicks.net>
2018-04-04 15:26:02 -0400

    Implement `generate:indexes`

--- src/commands/generate/indexes.ts
+++ src/commands/generate/indexes.ts
@@ -1,9 +1,12 @@
+import * as lib from "../../lib";
 import * as oclif from "@oclif/command";
 
 export default class GenerateIndexes extends oclif.Command {
     static description = "generate index files";
-
+    static aliases = ["generate"];
     async run() {
-        // TODO implement
+        const generator = new lib.IndexGenerator(lib.WORKING_DIR);
+        await generator.generate();
+        this.log("Generated indexes.");
     }
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,5 @@ export * from "./constants";
 export * from "./EtaProcess";
 export { default as eta } from "./helpers/eta";
 export { default as fs } from "./helpers/fs";
+export { default as IndexGenerator } from "./helpers/indexes";
 export * from "./helpers/promisified";
