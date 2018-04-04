490d354d06cbab39dc00375bebea8f98233e099d
Alex Hicks <alex@alexhicks.net>
2018-04-04 15:25:49 -0400

    Add recursive-readdir promisification

--- src/lib/helpers/promisified.ts
+++ src/lib/helpers/promisified.ts
@@ -1,8 +1,10 @@
 import * as childProcess from "child_process";
 import * as readline from "readline";
+import * as recursiveReaddirCallback from "recursive-readdir";
 import * as util from "util";
 
 export const exec = util.promisify(childProcess.exec);
+export const recursiveReaddir: (path: string) => Promise<string[]> = <any>util.promisify(recursiveReaddirCallback);
 
 export function question(query: string): Promise<string> {
     const rl = readline.createInterface({
