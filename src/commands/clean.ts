e6bed910d7190ebc4861cbca07411e08d2255c12
Alex Hicks <alex@alexhicks.net>
2018-04-06 09:34:36 -0400

    Implement `clean`

---
+++ src/commands/clean.ts
@@ -0,0 +1,16 @@
+import * as fs from "fs-extra";
+import * as lib from "../lib";
+import * as oclif from "@oclif/command";
+
+export default class Clean extends oclif.Command {
+    static description = "clean all JS files without matching TS files";
+
+    async run() {
+        const files = (await lib.recursiveReaddir(lib.WORKING_DIR))
+            .map(f => f.replace(/\\/g, "/"))
+            .filter(f => !f.includes("/node_modules/"));
+        const unmatchedFiles = files.filter(f => f.endsWith(".js") && !files.includes(f.replace(/\.js/g, ".ts")));
+        await Promise.all(unmatchedFiles.map(f => fs.unlink(f)));
+        this.log(`Deleted ${unmatchedFiles.length} unmatched files.`);
+    }
+}
