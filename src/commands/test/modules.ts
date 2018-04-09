4b4243e7c8ee704919d5cbbea2da898ab0ee0a8f
Alex Hicks <alex@alexhicks.net>
2018-04-09 07:04:27 -0400

    Clean up `test:modules` and add some documentation

--- src/commands/test/modules.ts
+++ src/commands/test/modules.ts
@@ -39,9 +39,10 @@ export default class TestModules extends oclif.Command {
         if (flags.reset) await DBSeed.seed(server);
         const mocha = new Mocha({
             slow: Number(flags.slow),
-            timeout: Number(flags.reset)
+            timeout: Number(flags.reset),
         });
         const modulesDir = lib.WORKING_DIR + "/modules/";
+        // add all files in test/ and modules/*/test/ to Mocha
         (await Promise.all((await fs.readdir(modulesDir)).map(async d => {
             const testDir = modulesDir + d + "/test";
             if (!await fs.pathExists(testDir)) return [];
@@ -49,21 +50,22 @@ export default class TestModules extends oclif.Command {
         }))).reduce((p, v) => p.concat(v), []).filter(f => f.endsWith(".js")).forEach(f => mocha.addFile(f));
         const db = await lib.eta.connectDatabase(lib.WORKING_DIR);
         let apiToken = "";
-        if (flags.reset) {
+        if (flags.reset) { // add a new API token to fresh DB
             const result = await db.query(`select user_id as id from user_position where start <= current_date and ("end" is null or "end" > current_date) limit 1`);
             apiToken = crypto.randomBytes(16).toString("hex");
             await db.query(`update "user" set "api_token" = $1::text where "id" = $2::int`, [apiToken, result.rows[0].id]);
-        } else {
+        } else { // get the previously-set API token
             const result = await db.query(`select api_token as "apiToken" from "user" where api_token is not null limit 1`);
             apiToken = result.rows[0].apiToken;
         }
         require(lib.WORKING_DIR + "/helpers/require.js"); // set up support for require("@eta/...")
         process.env.API_TOKEN = apiToken;
+        await db.end();
         await new Promise(resolve => {
             mocha.run(() => {
-                server.process!.kill("SIGINT");
                 resolve();
             });
         });
+        server.process!.kill();
     }
 }
