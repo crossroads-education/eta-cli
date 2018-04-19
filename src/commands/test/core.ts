f7f5dec3a03fa5fe5541842d43876f40e2122963
Alex <alex@alexhicks.net>
2018-04-18 21:39:13 -0400

    Implement more robust `test:core`

--- src/commands/test/core.ts
+++ src/commands/test/core.ts
@@ -1,12 +1,12 @@
 import * as lib from "../../lib";
+import * as Mocha from "mocha";
 import * as oclif from "@oclif/command";
 
 export default class TestCore extends oclif.Command {
-    static description = "run Eta's unit/integration tests";
+    static description = "run Eta's unit tests";
     static flags = {
-        "log-standard-output": oclif.flags.boolean({
-            char: "l",
-            description: "Write normal Mocha output to console (instead of CLI output)",
+        "reporter": oclif.flags.string({
+            description: "reporter name for mocha to use",
             required: false
         })
     };
@@ -14,27 +14,12 @@ export default class TestCore extends oclif.Command {
 
     async run() {
         const { flags } = this.parse(TestCore);
-        if (!flags["log-standard-output"]) this.log("Running tests on Eta core...");
-        let result: {
-            stdout: string;
-            stderr: string;
-        };
-        try {
-            result = await lib.exec(`node ${lib.MOCHA_PATH} --recursive`, {
-                cwd: lib.WORKING_DIR,
-                env: {
-                    "ETA_logger_outputToConsole": false
-                }
-            });
-        } catch (err) {
-            process.stderr.write(err.stdout);
-            return;
-        }
-        if (result.stderr) {
-            process.stderr.write(result.stderr);
-            return;
-        }
-        if (flags["log-standard-output"]) process.stdout.write(result.stdout);
-        else this.log("All tests passed.");
+        const mocha = new Mocha();
+        if (flags.reporter) mocha.reporter(flags.reporter);
+        (await lib.recursiveReaddir(lib.WORKING_DIR + "/test")).filter(f => f.endsWith(".js"))
+            .forEach(f => mocha.addFile(f));
+        const failures = await new Promise<number>(resolve =>
+            mocha.run(failures => resolve(failures)));
+        if (failures > 0) this.exit(1);
     }
 }
